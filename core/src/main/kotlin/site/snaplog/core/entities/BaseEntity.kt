d416cd71768f2216b846d0481957b85c67071914
송준섭 <wnstjq7979@gmail.com>
2024-09-27 21:35:49 +0900

    [#2] feat: BaseEntity 추가

---
+++ core/src/main/kotlin/site/snaplog/core/entities/BaseEntity.kt
@@ -0,0 +1,16 @@
+package site.snaplog.core.entities
+
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.relational.core.mapping.Column
+import java.time.LocalDateTime
+
+open class BaseEntity(
+    @CreatedDate
+    @Column("created_at")
+    val createdAt: LocalDateTime? = null,
+
+    @LastModifiedDate
+    @Column("updated_at")
+    val updatedAt: LocalDateTime? = null
+)
