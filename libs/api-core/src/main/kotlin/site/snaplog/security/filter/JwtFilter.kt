526beff8adf2ed1891fc4f631818a523590fec03
SongJSeop <wnstjq7979@gmail.com>
2024-10-10 01:11:08 +0900

    [#18] fix: 하위 모듈들 설정 정보 읽어오지 못하는 문제 해결

--- libs/api-core/src/main/kotlin/site/snaplog/security/filter/JwtFilter.kt
+++ libs/api-core/src/main/kotlin/site/snaplog/security/filter/JwtFilter.kt
@@ -1,7 +1,5 @@
 package site.snaplog.security.filter
 
-import org.springframework.core.Ordered
-import org.springframework.core.annotation.Order
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.ReactiveSecurityContextHolder
 import org.springframework.stereotype.Component
