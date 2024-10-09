bc35045bfa85e9ca4f3832099b4f84b3752a2274
SongJSeop <wnstjq7979@gmail.com>
2024-10-10 00:39:05 +0900

    [#8] chore: api-core 모듈 필요 의존성 추가

--- libs/api-core/build.gradle.kts
+++ libs/api-core/build.gradle.kts
@@ -8,6 +8,17 @@ jar.enabled = true
 
 dependencies {
     implementation(project(":common"))
+    implementation(project(":mysql"))
+    implementation(project(":redis"))
 
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
+
+    // jwt
+    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
 }
