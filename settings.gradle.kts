233228b3e1b544f138197cbfd7cbac757c89d818
SongJSeop <wnstjq7979@gmail.com>
2024-10-09 17:46:17 +0900

    [#6] init: mysql 모듈 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,3 +7,11 @@ listOf(
     include(it)
     project(":$it").projectDir = File("$rootDir/libs/$it")
 }
+
+// libs/infra 하위 모듈 세팅
+listOf(
+    "mysql"
+).forEach {
+    include(it)
+    project(":$it").projectDir = File("$rootDir/libs/infra/$it")
+}
