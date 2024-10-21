e532de3b62aa0b5196449c855999233e1af004b2
SongJSeop <wnstjq7979@gmail.com>
2024-10-21 23:01:32 +0900

    [#25] feat: LoginResponseDto 추가

---
+++ apps/api/snaplog-api/src/main/kotlin/site/snaplog/dto/auth/response/LoginResponseDto.kt
@@ -0,0 +1,12 @@
+package site.snaplog.dto.auth.response
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(description = "로그인 응답 DTO")
+data class LoginResponseDto(
+    @field:Schema(description = "AccessToken")
+    val accessToken: String,
+
+    @field:Schema(description = "RefreshToken")
+    val refreshToken: String
+)
